a949ed8c28d2f11181509d2b1902dfe4f16fb5b8
Junaid Farooq <junaid@evercam.io>
2021-11-14 01:24:02 +0500

    Start saving sessions.

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-import qrcode from "qrcode-terminal"
+const fs = require('fs');
 
-import {
-  Client
-} from "whatsapp-web.js"
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
 
-const client = new Client();
+const SESSION_FILE_PATH = './session.json';
+
+let sessionData;
+if(fs.existsSync(SESSION_FILE_PATH)) {
+  sessionData = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({
+  session: sessionData
+});
 
 client.on('qr', qr => {
-  qrcode.generate(qr, {
-    small: true
-  });
+  qrcode.generate(qr, {small: true});
 });
 
 client.on('ready', () => {
   console.log('Client is ready!');
 });
 
-client.initialize();
-
 client.on('message', message => {
-  console.log(message.body);
+	if(message.body === '!ping') {
+		message.reply('pong');
+	}
 });
 
-client.on('message', message => {
-  if (message.body === '!ping') {
-    message.reply('pong');
-  }
-})
+client.on('authenticated', (session) => {
+  sessionData = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
+
+client.initialize();
