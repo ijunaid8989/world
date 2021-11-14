67d8152a421e0b15ea86b41e90f9b2096b08fcc6
Junaid Farooq <junaid@evercam.io>
2021-11-14 15:43:37 +0500

    Resume or start empty session.

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ const { Client } = require('whatsapp-web.js');
 const SESSION_FILE_PATH = './session.json';
 
 let sessionData;
-if(fs.existsSync(SESSION_FILE_PATH)) {
+
+if (fs.existsSync(SESSION_FILE_PATH)) {
   sessionData = require(SESSION_FILE_PATH);
 }
 
-const client = new Client({
-  session: sessionData
-});
+console.log(sessionData)
+
+let client = null
+
+if (Object.keys(sessionData).length === 0) {
+  client = new Client()
+} else {
+  client = new Client({
+    session: sessionData
+  })
+}
 
 client.on('qr', qr => {
   qrcode.generate(qr, {small: true});
